8dc2ac41b839b5507faf8d473fe4c8a554f78956
LENOVO <LENOVO@DESKTOP-HNVMAVD>
2024-08-04 10:17:37 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace _01_Deber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lista_tareas = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Materia_text = new System.Windows.Forms.TextBox();
+            this.tarea_text = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Wide Latin", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(212, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1340, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gestión de Tareas Pendientes";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Lista_tareas
+            // 
+            this.Lista_tareas.FormattingEnabled = true;
+            this.Lista_tareas.ItemHeight = 20;
+            this.Lista_tareas.Location = new System.Drawing.Point(710, 98);
+            this.Lista_tareas.Name = "Lista_tareas";
+            this.Lista_tareas.Size = new System.Drawing.Size(1018, 524);
+            this.Lista_tareas.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Materia: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(95, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tarea:";
+            // 
+            // Materia_text
+            // 
+            this.Materia_text.Location = new System.Drawing.Point(209, 179);
+            this.Materia_text.Name = "Materia_text";
+            this.Materia_text.Size = new System.Drawing.Size(402, 26);
+            this.Materia_text.TabIndex = 1;
+            // 
+            // tarea_text
+            // 
+            this.tarea_text.Location = new System.Drawing.Point(209, 261);
+            this.tarea_text.Multiline = true;
+            this.tarea_text.Name = "tarea_text";
+            this.tarea_text.Size = new System.Drawing.Size(402, 183);
+            this.tarea_text.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(848, 659);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1380, 659);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(202, 43);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1766, 756);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tarea_text);
+            this.Controls.Add(this.Materia_text);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Lista_tareas);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox Lista_tareas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Materia_text;
+        private System.Windows.Forms.TextBox tarea_text;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_Deber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var materia = Materia_text.Text;
+            var tarea = tarea_text.Text;
+
+            if (materia != "" && tarea !="") { 
+
+                string cadena = materia + " " + tarea;
+
+                Lista_tareas.Items.Add(cadena);
+                LimpiarCajas();
+                MessageBox.Show("Se agrego correctamente");
+
+
+            }
+            else
+            {
+                MessageBox.Show("Complete los Campos");
+            }
+
+
+
+          
+        }
+    
+        public void LimpiarCajas() {
+            tarea_text.Text = "";
+            Materia_text.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Lista_tareas.SelectedIndex != -1)
+            {
+                DialogResult dialogResult= MessageBox.Show ("Esta seguro que quiere eliminar la tarea " ,"" , MessageBoxButtons.YesNo); 
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Lista_tareas.Items.RemoveAt(Lista_tareas.SelectedIndex);
+                    Lista_tareas.Items.Remove(Lista_tareas.SelectedIndex);
+                    MessageBox.Show("Se elimino con exito");
+                }
+                else
+                {
+                    MessageBox.Show("Se canceló con exito");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Operación invalida");
+            }
+        }
+    }
+}
+
